6d5a333c704501298e1d2f8291a0ce1320cb2211
Raphael <raphael.goetz.7@gmail.com>
2024-07-23 09:20:31 +0200

    feat: migrated from prost to tonic build configuration

--- build/rust/build.rs
+++ build/rust/build.rs
@@ -1,20 +1,23 @@
 use std::io::Result;
-use prost_build::*;
 
 fn main() -> Result<()> {
     let internal_prefix = "../../internal";
-    let mut config = Config::new();
 
-    config.protoc_arg("--proto_path=../../internal");
-
-    config.compile_protos(&[
-        format!("{internal_prefix}/configuration.proto"), 
-        format!("{internal_prefix}/flow.proto"), 
-        format!("{internal_prefix}/node.proto"), 
-        format!("{internal_prefix}/rule.proto"), 
-        format!("{internal_prefix}/type.proto"), 
+    let paths = &[
+        format!("{internal_prefix}/configuration.proto"),
+        format!("{internal_prefix}/flow.proto"),
+        format!("{internal_prefix}/node.proto"),
+        format!("{internal_prefix}/rule.proto"),
+        format!("{internal_prefix}/type.proto"),
         format!("{internal_prefix}/variable.proto")
-    ], &["src/lib.rs"]).expect("Cannot compile protos");
+    ];
+
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .protoc_arg("--proto_path=../../internal")
+        .compile(paths, &["src/lib.rs"])
+        .expect("Cannot compile protos");
 
     Ok(())
 }
