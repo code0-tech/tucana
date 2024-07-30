62e833b189825005d4bd783bbb8b6ebdeb1bd2c2
Niklas van Schrick <mc.taucher2003@gmail.com>
2024-07-30 22:36:19 +0200

    Add task to update version number of gem

--- build/ruby/lib/tucana/version.rb
+++ build/ruby/lib/tucana/version.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# this file is managed with the "release:version" task.
+# to update the version, run "bundle exec rake release:version[NEW_VERSION]".
+
 module Tucana
   VERSION = "0.0.0"
 end
