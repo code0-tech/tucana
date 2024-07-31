009c03a868b75e1758df5e28bd867c174a5901ba
Raphael <raphael.goetz.7@gmail.com>
2024-07-31 08:22:41 +0200

    fix: simplified build script

--- build/rust/build.rs
+++ build/rust/build.rs
@@ -1,28 +1,25 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    let internal_prefix = "../../internal";
-
     let paths = &[
-        format!("{internal_prefix}/flow.proto"),
-        format!("{internal_prefix}/node.proto"),
-        format!("{internal_prefix}/rule.proto"),
-        format!("{internal_prefix}/type.proto"),
-        format!("{internal_prefix}/variable.proto")
+        "flow.proto",
+        "node.proto",
+        "rule.proto",
+        "type.proto",
+        "variable.proto"
     ];
 
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
         .type_attribute("Variable", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .type_attribute("Node", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .type_attribute("Parameter", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .type_attribute("Rule", "#[derive(serde::Serialize, serde::Deserialize)]")
         .type_attribute("RuleType", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .type_attribute("Rule", "#[derive(serde::Serialize, serde::Deserialize)]")
         .type_attribute("Type", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .type_attribute("Parameter", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .type_attribute("Node", "#[derive(serde::Serialize, serde::Deserialize)]")
         .type_attribute("Flow", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .protoc_arg("--proto_path=../../internal")
-        .compile(paths, &["src/lib.rs"])
+        .compile(paths, &["../../internal"])
         .expect("Cannot compile protos");
 
     Ok(())
