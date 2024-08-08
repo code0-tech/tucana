fd1a3033850738592573b92c25c1875273bf3e73
Niklas van Schrick <mc.taucher2003@gmail.com>
2024-08-08 19:56:23 +0200

    Remove load path modification

--- build/ruby/lib/tucana.rb
+++ build/ruby/lib/tucana.rb
@@ -8,7 +8,6 @@ module Tucana
   def self.load_protocol(protocol)
     this_dir = File.expand_path(File.dirname(__FILE__))
     protocol_dir = File.join(this_dir, "tucana/#{protocol}")
-    $LOAD_PATH.unshift(protocol_dir) unless $LOAD_PATH.include?(protocol_dir)
 
     Dir["#{protocol_dir}/*_pb.rb"].each { |file| require file }
   end
