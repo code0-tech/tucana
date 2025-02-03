8494cb132c2d3d30e6cbf3be2f60dd048cdf936d
Raphael <raphael.goetz.7@gmail.com>
2025-02-03 10:05:10 +0100

    feat: added tests

--- build/rust/src/lib.rs
+++ build/rust/src/lib.rs
@@ -12,3 +12,54 @@ pub mod aquila {
 pub mod shared {
     include!("generated/shared.rs");
 }
+
+#[cfg(test)]
+#[cfg(feature = "sagittarius")]
+pub mod tests {
+    use crate::sagittarius::Flow;
+    use serde_json;
+
+    #[test]
+    fn test_serialize() {
+        let flow = Flow {
+            flow_id: 0,
+            r#type: "no".to_string(),
+            settings: vec![],
+            starting_node: None,
+        };
+
+        let str_flow = serde_json::to_string(&flow).expect("Serialization failed");
+        let json_flow: serde_json::Value = serde_json::from_str(&str_flow).expect("Failed to parse JSON");
+
+        let expected_json: serde_json::Value = serde_json::json!({
+            "flow_id": 0,
+            "type": "no",
+            "settings": [],
+            "starting_node": null
+        });
+
+        assert_eq!(json_flow, expected_json);
+    }
+
+    #[test]
+    fn test_deserialize() {
+        let json_data = r#"{
+            "flow_id": 0,
+            "type": "no",
+            "settings": [],
+            "starting_node": null
+        }"#;
+
+        let deserialized: Result<Flow, _> = serde_json::from_str(json_data);
+        assert!(deserialized.is_ok());
+
+        let expected_flow = Flow {
+            flow_id: 0,
+            r#type: "no".to_string(),
+            settings: vec![],
+            starting_node: None,
+        };
+
+        assert_eq!(deserialized.unwrap(), expected_flow);
+    }
+}
