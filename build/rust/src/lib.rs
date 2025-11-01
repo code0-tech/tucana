38a32926ed192f434758052c4090268effad30c8
raphael-goetz <raphael.goetz.7@gmail.com>
2025-11-01 10:44:55 +0100

    feat: included de/serialization into module export

--- build/rust/src/lib.rs
+++ build/rust/src/lib.rs
@@ -2,85 +2,17 @@ pub mod shared {
     pub mod helper;
 
     include!("generated/shared.rs");
-
-    #[cfg(test)]
-    pub mod tests {
-        use crate::shared::ValidationFlow;
-        use serde_json;
-
-        #[test]
-        fn test_serialize() {
-            let flow = ValidationFlow {
-                flow_id: 0,
-                project_id: 0,
-                data_types: vec![],
-                input_type_identifier: None,
-                return_type_identifier: None,
-                r#type: "no".to_string(),
-                settings: vec![],
-                starting_node_id: 0,
-                node_functions: vec![],
-            };
-
-            let str_flow = serde_json::to_string(&flow).expect("Serialization failed");
-            let json_flow: serde_json::Value =
-                serde_json::from_str(&str_flow).expect("Failed to parse JSON");
-
-            let expected_json: serde_json::Value = serde_json::json!({
-                "flow_id": 0,
-                "project_id": 0,
-                "type": "no",
-                "input_type_identifier": null,
-                "return_type_identifier": null,
-                "data_types": [],
-                "settings": [],
-                "starting_node_id": 0,
-                "node_functions": []
-            });
-
-            assert_eq!(json_flow, expected_json);
-        }
-
-        #[test]
-        fn test_deserialize() {
-            let json_data = r#"{
-                "flow_id": 0,
-                "project_id": 0,
-                "type": "no",
-                "input_type_identifier": null,
-                "return_type_identifier": null,
-                "data_types": [],
-                "settings": [],
-                "starting_node_id": 0,
-                "node_functions": []
-            }"#;
-
-            let deserialized: Result<ValidationFlow, _> = serde_json::from_str(json_data);
-            assert!(deserialized.is_ok());
-
-            let expected_flow = ValidationFlow {
-                flow_id: 0,
-                project_id: 0,
-                r#type: "no".to_string(),
-                settings: vec![],
-                data_types: vec![],
-                input_type_identifier: None,
-                return_type_identifier: None,
-                starting_node_id: 0,
-                node_functions: vec![],
-            };
-
-            assert_eq!(deserialized.unwrap(), expected_flow);
-        }
-    }
+    include!("generated/shared.serde.rs");
 }
 
 #[cfg(feature = "aquila")]
 pub mod aquila {
     include!("generated/aquila.rs");
+    include!("generated/aquila.serde.rs");
 }
 
 #[cfg(feature = "sagittarius")]
 pub mod sagittarius {
     include!("generated/sagittarius.rs");
+    include!("generated/sagittarius.serde.rs");
 }
